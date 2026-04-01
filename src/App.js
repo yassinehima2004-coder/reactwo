5a6e36a4d2c1313990bc3fcd028ad9bf72cbd2ad
yassinehima2004-coder <yassinehima2004@gmail.com>
2026-04-01 11:33:38 +0100

    Version final

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Home/>
+        <Footer/>
+    </div>
+
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './Header';
-import Home from './Home';
-import Footer from './Footer';
+import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Home/>
-    <Footer/>
+      <App />
   </React.StrictMode>
 );
 
